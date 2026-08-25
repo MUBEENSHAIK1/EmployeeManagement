31c47da40fe86158f080cfb4a93ece9e1fbd2b3d
shaik mubeen <mubeensk196@gmail.com>
2026-08-25 10:22:23 +0530

    Implement JDBC database connection and DAO

--- src/main/java/com/example/employeemanagement/dao/EmployeeDAO.java
+++ src/main/java/com/example/employeemanagement/dao/EmployeeDAO.java
@@ -10,21 +10,18 @@ import com.example.employeemanagement.model.Employee;
 
 public class EmployeeDAO {
 
-    private Connection connection;
-
-    public EmployeeDAO(Connection connection) {
-        this.connection = connection;
-    }
-
     public List<Employee> getAllEmployees() {
+
         List<Employee> employees = new ArrayList<>();
 
         String sql = "SELECT * FROM employee";
 
-        try (PreparedStatement statement = connection.prepareStatement(sql);
+        try (Connection connection = DBConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql);
              ResultSet resultSet = statement.executeQuery()) {
 
             while (resultSet.next()) {
+
                 Employee employee = new Employee();
 
                 employee.setId(resultSet.getInt("id"));
